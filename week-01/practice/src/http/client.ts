a5da71d36380ddf4ca47c271a388086e71410327
Allen <allentango@163.com>
2021-09-06 00:09:12 +0800

    update

--- week-01/practice/src/http/client.ts
+++ week-01/practice/src/http/client.ts
@@ -163,7 +163,37 @@ class TrunkedBodyParser {
     }
 
     receiveChar(char) {
-        // pass
+        if (this.current === this.WAITING_LENGTH) {
+            if(char === "\r") {
+                if (this.length === 0) {
+                    this.isFinished = true;
+                }
+                this.current = this.WAITING_NEW_LINE_END;
+            } else {
+                this.length *= 16;
+                this.length += parseInt(char, 16);
+            }
+        } else if (this.current === this.WAITING_NEW_LINE_END) {
+            if (char === "\n") {
+                this.current = this.READING_TRUNK;
+            }
+        } else if (this.current === this.READING_TRUNK) {
+            if (this.length) {
+                this.content.push(char);
+                this.length--;
+            }
+            if (this.length === 0) {
+                this.current = this.WAITING_NEW_LINE;
+            }
+        } else if (this.current === this.WAITING_NEW_LINE) {
+            if (char === "\r") {
+                this.current = this.WAITING_NEW_LINE_END;
+            }
+        } else if (this.current === this.WAITING_NEW_LINE_END) {
+            if (char === "\n") {
+                this.current = this.WAITING_LENGTH;
+            }
+        }
     }
 }
 
